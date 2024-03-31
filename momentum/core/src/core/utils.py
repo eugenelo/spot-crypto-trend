54ed6811f8256a9057535cf397d7fea2044c3234
Eugene Lo <eugene.lo@berkeley.edu>
2024-03-31 13:53:54 -0700

    Silence warning about filling NaNs (#8)

--- momentum/core/src/core/utils.py
+++ momentum/core/src/core/utils.py
@@ -19,30 +19,35 @@ def apply_hysteresis(
     df["below_exit"] = df[value_col] < exit_threshold
 
     # Determine points where state changes
-    df["entry_point"] = df["above_entry"] & (~df["above_entry"].shift(1).fillna(False))
-    df["exit_point"] = df["below_exit"] & (~df["below_exit"].shift(1).fillna(False))
+    with pd.option_context("future.no_silent_downcasting", True):
+        df["entry_point"] = df["above_entry"] & (
+            ~df["above_entry"].shift(1).fillna(False).astype(bool)
+        )
+        df["exit_point"] = df["below_exit"] & (
+            ~df["below_exit"].shift(1).fillna(False).astype(bool)
+        )
 
-    # Ensure group changes reset entry/exit points
-    df["group_change"] = df[group_col] != df[group_col].shift(1)
-    df["entry_point"] |= df["group_change"]
-    df["exit_point"] &= ~df["group_change"]
+        # Ensure group changes reset entry/exit points
+        df["group_change"] = df[group_col] != df[group_col].shift(1)
+        df["entry_point"] |= df["group_change"]
+        df["exit_point"] &= ~df["group_change"]
 
-    # Initialize hysteresis column
-    df[output_col] = np.nan
+        # Initialize hysteresis column, use pd.NA to set dtype = object
+        df[output_col] = pd.NA
 
-    # Apply hysteresis logic: set to True at entry points and propagate until an exit point within each group
-    df.loc[df["entry_point"], output_col] = True
-    df.loc[df["exit_point"], output_col] = False
+        # Apply hysteresis logic: set to True at entry points and propagate until an exit point within each group
+        df.loc[df["entry_point"], output_col] = True
+        df.loc[df["exit_point"], output_col] = False
 
-    # Forward fill within groups to propagate state, then backward fill initial NaNs if any
-    df[output_col] = df.groupby(group_col)[output_col].ffill().bfill()
+        # Forward fill within groups to propagate state, then backward fill initial NaNs if any
+        df[output_col] = df.groupby(group_col)[output_col].ffill().bfill().astype(bool)
 
-    # Drop helper columns
-    df.drop(
-        ["above_entry", "below_exit", "entry_point", "exit_point", "group_change"],
-        axis=1,
-        inplace=True,
-    )
+        # Drop helper columns
+        df.drop(
+            ["above_entry", "below_exit", "entry_point", "exit_point", "group_change"],
+            axis=1,
+            inplace=True,
+        )
 
     return df
 
